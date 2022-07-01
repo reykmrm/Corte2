79ace5f0a4251d54a5affb617ef3478d77f50a1c
Reyk Mrm <8428349+reykmrm@users.noreply.github.com>
2022-06-30 22:03:45 -0500

    tabla del 2

---
+++ javaScripp.js
@@ -0,0 +1,38 @@
+/** @format */
+
+const operdador = 2;
+let operando = 1;
+let result = 0;
+
+result = operdador * operando;
+console.log(result)
+
+
+
+operando = operando +1
+result = operdador * (operando )
+console.log(result)
+
+operando = operando +1
+result = operdador * (operando)
+console.log(result)
+
+operando = operando +1
+result = operdador * (operando)
+console.log(result)
+
+operando = operando +1
+result = operdador * (operando)
+console.log(result)
+
+operando = operando +1
+result = operdador * (operando)
+console.log(result)
+
+operando = operando +1
+result = operdador * (operando)
+console.log(result)
+
+operando = operando +1
+result = operdador * (operando)
+console.log(result)
